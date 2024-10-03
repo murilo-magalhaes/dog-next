c2b2b9224068d7b4c3e259d177961da7f266cccc
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-03 17:45:39 -0300

    feat: create menu component and learn metadata

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Contato',
+  description: 'Essa é a página contato'
+}
+
+export default function ConattoPage() {
+  return (
+    <main>
+      <h2>Contato</h2>
+    </main>
+  );
+}
---
+++ src/components/menu.tsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+  return <ul className="menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="/sobre">Sobre</a></li>
+    <li><a href="/contato">Contato</a></li>
+  </ul>
+}
