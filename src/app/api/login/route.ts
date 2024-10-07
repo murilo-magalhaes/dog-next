76fa357e65c26e0e426c360f94b8473a68b5381a
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-07 09:31:21 -0300

    feat: login routine with cookies to save token

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,24 @@
+import { cookies } from 'next/headers';
+
+export async function GET() {
+  const response = await fetch('https://api.origamid.online/conta/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: 'dog',
+      password: 'dog',
+    }),
+  });
+
+  if (!response.ok) return Response.json({ erro: 'Erro na api' });
+
+  const data = await response.json();
+  cookies().set('token', data.token, {
+    httpOnly: true,
+    secure: true,
+  });
+
+  return Response.json(data);
+}
